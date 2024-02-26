40f79649212575875629a6f3c2b2bce0e4b5b71a
loopnyapy <da.eto.deniska@gmail.com>
2024-02-26 14:04:38 +0300

    feat: new action types update: version

--- dart/lib/proto/action_type.pbenum.dart
+++ dart/lib/proto/action_type.pbenum.dart
@@ -15,29 +15,27 @@ import 'package:protobuf/protobuf.dart' as $pb;
 
 class ActionType extends $pb.ProtobufEnum {
   static const ActionType ACTION_TYPE_UNKNOWN = ActionType._(0, _omitEnumNames ? '' : 'ACTION_TYPE_UNKNOWN');
-  static const ActionType ACTION_TYPE_PUBLISH = ActionType._(1, _omitEnumNames ? '' : 'ACTION_TYPE_PUBLISH');
-  static const ActionType ACTION_TYPE_MARK_PAID = ActionType._(2, _omitEnumNames ? '' : 'ACTION_TYPE_MARK_PAID');
-  static const ActionType ACTION_TYPE_AUDIT = ActionType._(3, _omitEnumNames ? '' : 'ACTION_TYPE_AUDIT');
+  static const ActionType ACTION_TYPE_CONFIRM_ENTRY = ActionType._(1, _omitEnumNames ? '' : 'ACTION_TYPE_CONFIRM_ENTRY');
+  static const ActionType ACTION_TYPE_AUDIT = ActionType._(2, _omitEnumNames ? '' : 'ACTION_TYPE_AUDIT');
+  static const ActionType ACTION_TYPE_ALLOW_PAYOUT = ActionType._(3, _omitEnumNames ? '' : 'ACTION_TYPE_ALLOW_PAYOUT');
   static const ActionType ACTION_TYPE_PAYOUT = ActionType._(4, _omitEnumNames ? '' : 'ACTION_TYPE_PAYOUT');
-  static const ActionType ACTION_TYPE_ALLOW_PAYOUT = ActionType._(5, _omitEnumNames ? '' : 'ACTION_TYPE_ALLOW_PAYOUT');
-  static const ActionType ACTION_TYPE_CONFIRM = ActionType._(6, _omitEnumNames ? '' : 'ACTION_TYPE_CONFIRM');
-  static const ActionType ACTION_TYPE_CANCEL = ActionType._(7, _omitEnumNames ? '' : 'ACTION_TYPE_CANCEL');
+  static const ActionType ACTION_TYPE_MARK_PAID = ActionType._(5, _omitEnumNames ? '' : 'ACTION_TYPE_MARK_PAID');
+  static const ActionType ACTION_TYPE_DISALLOW_PAYOUT = ActionType._(6, _omitEnumNames ? '' : 'ACTION_TYPE_DISALLOW_PAYOUT');
+  static const ActionType ACTION_TYPE_CANCEL_AUDIT = ActionType._(7, _omitEnumNames ? '' : 'ACTION_TYPE_CANCEL_AUDIT');
   static const ActionType ACTION_TYPE_CANCEL_CONFIRMATION = ActionType._(8, _omitEnumNames ? '' : 'ACTION_TYPE_CANCEL_CONFIRMATION');
-  static const ActionType ACTION_TYPE_CANCEL_AUDIT = ActionType._(9, _omitEnumNames ? '' : 'ACTION_TYPE_CANCEL_AUDIT');
-  static const ActionType ACTION_TYPE_DISALLOW_PAYOUT = ActionType._(10, _omitEnumNames ? '' : 'ACTION_TYPE_DISALLOW_PAYOUT');
+  static const ActionType ACTION_TYPE_CANCEL = ActionType._(9, _omitEnumNames ? '' : 'ACTION_TYPE_CANCEL');
 
   static const $core.List<ActionType> values = <ActionType> [
     ACTION_TYPE_UNKNOWN,
-    ACTION_TYPE_PUBLISH,
-    ACTION_TYPE_MARK_PAID,
+    ACTION_TYPE_CONFIRM_ENTRY,
     ACTION_TYPE_AUDIT,
-    ACTION_TYPE_PAYOUT,
     ACTION_TYPE_ALLOW_PAYOUT,
-    ACTION_TYPE_CONFIRM,
-    ACTION_TYPE_CANCEL,
-    ACTION_TYPE_CANCEL_CONFIRMATION,
-    ACTION_TYPE_CANCEL_AUDIT,
+    ACTION_TYPE_PAYOUT,
+    ACTION_TYPE_MARK_PAID,
     ACTION_TYPE_DISALLOW_PAYOUT,
+    ACTION_TYPE_CANCEL_AUDIT,
+    ACTION_TYPE_CANCEL_CONFIRMATION,
+    ACTION_TYPE_CANCEL,
   ];
 
   static final $core.Map<$core.int, ActionType> _byValue = $pb.ProtobufEnum.initByValue(values);
