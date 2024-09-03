bcb1e37cfae30c06094978b5145c8d245382f2e7
loopnyapy <da.eto.deniska@gmail.com>
2024-09-03 16:18:49 +0300

    update: pubpsec and setup.py package version

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dictum-proto',
-    version='0.5.0-alpha.1',
+    version='0.5.0',
     packages=find_packages(),
     install_requires=[
         'protobuf',
