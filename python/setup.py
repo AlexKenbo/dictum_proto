476a6e1d3a66a6f86fd60f2b4c3673229220a8ca
loopnyapy <da.eto.deniska@gmail.com>
2024-10-17 13:12:04 +0300

    update: version 0.6.0 in pubspec and setup

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dictum-proto',
-    version="0.6.0-alpha.2",
+    version="0.6.0",
     packages=find_packages(),
     install_requires=[
         'protobuf',
